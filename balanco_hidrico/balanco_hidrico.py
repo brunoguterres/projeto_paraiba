1671f935925c2f1597f2a2759fc6c288c3e6fe94
beatiemi <beatiemik@gmail.com>
2023-11-10 17:32:46 -0300

    Atualizações no código de balanço hídrico

--- balanco_hidrico/balanco_hidrico.py
+++ balanco_hidrico/balanco_hidrico.py
@@ -1,4 +1,3 @@
-import numpy as np
 import psycopg2
 import time
 
@@ -10,7 +9,8 @@ def calculo_balanco(matriz): #esta função realiza o cálculo do balanço
             for j in range(i-1,-1,-1):
                 contador_montante = 0
                 if matriz[i][campo_cotrecho] == matriz[j][campo_trechojus] :
-                    matriz[i][campo_vazao_montante] += float(matriz[j][campo_vazao_jusante])
+                    a = float(matriz[i][campo_vazao_montante])
+                    a += float(matriz[j][campo_vazao_jusante])
                     contador_montante += 1
                     if contador_montante == 2:
                         break
@@ -19,13 +19,27 @@ def calculo_balanco(matriz): #esta função realiza o cálculo do balanço
                
     return matriz.tolist()
 
+def calculo_balanco_novo(dados): #esta função realiza o cálculo do balanço
+    for i in range(len(dados)):
+        if dados[i][campo_cabeceira] == '1':
+            dados[i][campo_vazao_montante] = 0
+        elif dados[i][campo_cabeceira] == '0':
+            for j in range(len(dados)): # colocar um teste para finalizar laço após encontrar 2 COBACIAJUS
+                if dados[i][campo_cotrecho] == dados[j][campo_trechojus] :
+                    dados[i][campo_vazao_montante] = dados[i][campo_vazao_montante]+dados[j][campo_vazao_jusante]
+        dados[i][campo_vazao_jusante] = dados[i][campo_vazao_montante]+float(dados[i][campo_disponibilidade])-float(dados[i][campo_captacao])
+        if dados[i][campo_vazao_jusante] < 0:
+            dados[i][campo_vazao_jusante] = 0
+            dados[i][campo_deficit] = dados[i][campo_vazao_jusante]*-1
+    return dados
+
 ##### EXECUÇÃO #####
 # Parâmetros de conexão com o banco de dados
-nome_do_banco = 'teste_tabela'
+nome_do_banco = 'banco_teste'
 usuario = 'postgres'
-senha = 'tonico'
+senha = 'cobrape'
 host = 'localhost'
-porta = '5432'
+porta = '5433'
 
 conn = psycopg2.connect(
     dbname = nome_do_banco,
@@ -35,15 +49,16 @@ conn = psycopg2.connect(
     port = porta
 )
 
-campo_cotrecho = 0  # vai ser o cotrecho
-campo_cobacia = 1 # vai ser a cobacia
-campo_trechojus = 2  # vai ser o ntutrjusante
-campo_disponibilidade = 3  # vai ser a disponibilidade no caso 1
-campo_cabeceira = 4 # cabeceira
-campo_vazao_montante = 5 # montante
-campo_vazao_jusante = 6  # jusante
-campo_deficit = 7   # vazio
-campo_valor_deficit = 8  # vazio
+campo_cotrecho = 0
+campo_cobacia = 1
+campo_trechojus = 2
+campo_trecho = 3
+campo_cabeceira = 4
+campo_vazao_montante = 5
+campo_vazao_jusante = 6
+campo_captacao = 7
+campo_disponibilidade = 8
+campo_deficit = 9
 
 print('Lendo:', time.time())
 # Ler o arquivo do BD e transformar em matriz (usa o numpy)
@@ -51,22 +66,24 @@ print('Lendo:', time.time())
 cursor = conn.cursor()
 cursor.execute("""
 SELECT *
-FROM parana50000
+FROM parana50000b
+ORDER BY cobacia DESC
+LIMIT 1
 """)
 
 # Converte os dados da tabela em uma lista de listas
 dados = []
+print(dados)
 for row in cursor:
-    dados.append(row)
+    lista_row = list(row)
+    dados.append(lista_row)
 
-# Transforma a lista de listas em uma matriz
-matriz = np.array(dados)
-print('Calculando:', time.time())
+print(dados)
 
 # Fazer o balanço ()
 
-matriz = calculo_balanco(matriz)
-
+dados = calculo_balanco_novo(dados)
+'''
 #atualizar o BD
 t1 = (time.time())
 print('Escrevendo:', t1)
@@ -87,6 +104,6 @@ conn.close()
 
 t2 = (time.time())
 print('Escrevendo:', t2)
-
+'''
 
 print('Banco de dados atualizado !!!')
